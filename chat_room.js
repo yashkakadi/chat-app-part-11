99eac8438a3fe3c33e7f109a8d656f525cc6a1f0
yashkakadi <143870757+yashkakadi@users.noreply.github.com>
2023-10-28 16:00:14 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBBIlg5UP3YapxdjRaqKhSlCJuh30gjfOo",
+    authDomain: "kwitter-66f6a.firebaseapp.com",
+    databaseURL: "https://kwitter-66f6a-default-rtdb.firebaseio.com",
+    projectId: "kwitter-66f6a",
+    storageBucket: "kwitter-66f6a.appspot.com",
+    messagingSenderId: "637783276523",
+    appId: "1:637783276523:web:7beafd33de4dd52242b7ae"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
